2bbe48667a4dd35c8e76a47679343bc46c205a5e
Christian Horton <christian@christianhorton.me>
2021-11-08 13:12:15 +0000

    Added hass_channel and list_channels

--- channel.c
+++ channel.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
+#include "user.h"
 
 struct channel_t {
     char *name;
     struct channel_t *next;
+    struct user_t* users;
 };
 
 static struct channel_t *head = 0;
 
-void add_channel(struct channel_t *channel) {
-    if(!head) head = channel;
+void list_channels() {
+    struct channel_t* curr = head;
+
+    while(curr) {
+        printf("[Channel %p] name=%s, next=%p\n", curr, curr->name, curr->next);
+
+        curr = curr->next;
+    }
+}
+
+void add_channel(struct channel_t* channel) {
+    if(!head) {
+        head = channel;
+        return;
+    }
 
     // Add to the top of the list
     channel->next = head;
     head = channel;
-
-    printf("Adding new channel\n", channel->name);
 }
 
-struct channel_t *get_channel(char *name) {
-    struct channel_t *curr = head;
+struct channel_t* get_channel(char *name) {
+    struct channel_t* curr = head;
 
     while(curr) {
-        
         if(curr->name == name) {
             return curr;
         }
+        curr = curr->next;
+    }
+    return 0;
+}
 
+int has_channel(struct channel_t* channel) {
+    struct channel_t* curr = head;
+
+    while(curr) {    
+        if(curr == channel) return 1;
         curr = curr->next;
     }
 
-    return 0
+    return 0;
 }
--- channel.h
+++ channel.h
@@ -1,8 +1,12 @@
 struct channel_t {
-    char *name;
-    struct channel_t *next;
+    char* name;
+    struct channel_t* next;
 };
 
-extern void add_channel(struct channel_t *channel);
+extern void list_channels();
 
-extern struct channel_t *get_channel(char *name);
+extern struct channel_t *get_channel(char* name);
+
+extern void add_channel(struct channel_t* channel);
+
+extern int has_channel(struct channel_t* channel);
