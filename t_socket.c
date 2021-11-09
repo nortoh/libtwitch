46ff87bce6f033bcc9b753ec8bc4938c528b17d6
Christian Horton <christian@christianhorton.me>
2021-11-09 18:54:05 +0000

    Completed socket threading and Twitch auth

--- t_socket.c
+++ t_socket.c
@@ -3,39 +3,19 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "utils.h"
+#include "config.h"
 
 static int sock = 0, running = 0;
-static char* buffer[1024];
+static char buffer[1024] = {0};
 static struct sockaddr_in serv_addr;
 
-void* thread_start(void *vargs) {
-    int count = 0;
-    while(running) {
-        if(count < 1000) {
-            printf("[%d]\n", count);
-            count++;
-            continue;
-        }
-        break;
-    }
-}
-
-int connect_to_twitch() {
-    // Socket thread identifier
-    pthread_t socket_thread;
-
-    running = 1;
-
-    info(__FILE__, "Creating socket thread\n");
-    if(pthread_create(&socket_thread, 0, thread_start, 0) != 0) {
-        error(__FILE__, "Could not create socket thread!!!\n");
-        return 0;
-    }
-
-    pthread_join(socket_thread, 0);
-
-    return 1;
+int raw(char* raw) {
+    if(!sock) return -1;
+    printf("> Sending %s", raw);
+    return send(sock, raw, strlen(raw), 0);
 }
 
 int conn(char* host, int port) {
@@ -67,7 +47,78 @@ int conn(char* host, int port) {
     return 1;
 }
 
-int raw(char* raw) {
-    if(!sock) return -1;
-    return send(sock, raw, strlen(raw), 0);
+void handle(char* raw) {
+    // Simple tokenizer
+    char* token;
+    char* result;
+    printf("Building %s\n", raw);
+    for(token = strtok_r(raw, " ", &result); token != 0; token = strtok_r(0, " ", &result)) {
+        printf("token: %s\n", token);
+    }
+    
+}
+
+void* thread_start(void *vargs) {
+
+    int connection = conn("44.226.36.141", 6667);
+
+    char* password;
+    char* username;
+    char* nickname;
+    
+    asprintf(&password, "PASS %s\r\n", get_config_value("oauth_key"));
+    asprintf(&nickname, "NICK %s\r\n", get_config_value("username"));
+    asprintf(&username, "USER %s\r\n", get_config_value("username"));
+    
+    // Caps
+    raw(password);
+    raw(nickname);
+    raw(username);
+    
+    if(connection < 0) {
+        error(__FILE__, "Error calling conn()\n");
+    }
+
+    int received = read(sock, buffer, 1024);
+    printf("< %s\n", buffer);
+
+    // Simple tokenizer
+    char* token;
+    char* result;
+    for(token = strtok_r(buffer, "\r\n", &result); token != 0; token = strtok_r(0, "\r\n", &result)) {
+        handle(token);
+    }
+    
+    
+    // do {
+    //     received = read(sock, buffer, 1024);
+    //     printf("< %s\n", buffer);
+    // } while(running);
+
+    // int count = 0;
+    // while(running) {
+    //     if(count < 1000) {
+    //         printf("[%d]\n", count);
+    //         count++;
+    //         continue;
+    //     }
+    //     break;
+    // }
+}
+
+int connect_to_twitch() {
+    // Socket thread identifier
+    pthread_t socket_thread;
+
+    running = 1;
+
+    info(__FILE__, "Creating socket thread\n");
+    if(pthread_create(&socket_thread, 0, thread_start, 0) != 0) {
+        error(__FILE__, "Could not create socket thread!!!\n");
+        return 0;
+    }
+
+    pthread_join(socket_thread, 0);
+
+    return 1;
 }
--- t_socket.h
+++ t_socket.h
@@ -1,3 +1,5 @@
 extern int connect_to_twitch();
 
+void handle(char* raw);
+
 extern int conn(char* host, int port);
