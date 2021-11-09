1323d497b8b72ce59c50068679ec31a557b80d68
Christian Horton <christian@christianhorton.me>
2021-11-09 20:13:47 +0000

    Added trim_whitespaces

--- utils.c
+++ utils.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void info(char* file, char* message) {
     printf("[%s] %s", file, message);
@@ -6,4 +7,18 @@ void info(char* file, char* message) {
 
 void erorr(char* file, char* message) {
     printf("!!! [%s] %s", file, message);
+}
+
+void trim_whitespaces(char* data) {
+    size_t len = strlen(data);
+
+    int i, j;
+    for(i = 0; i < len; i++) {
+        if(data[i] == ' ') {
+            for(j = 0; j < len; j++) {
+                data[j] = data[j + 1];
+            }
+            len--;
+        }
+    }
 }
--- utils.h
+++ utils.h
@@ -1,3 +1,5 @@
 extern void info(char* file, char* message);
 
 extern void error(char* file, char* message);
+
+extern void trim_whitespaces(char* data);
