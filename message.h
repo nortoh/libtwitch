71f61ebb1ec111a8b5c80d79c9c15703bdeb4014
Christian Horton <christian@christianhorton.me>
2022-02-28 03:12:30 +0000

    Fixed message.h

--- message.h
+++ message.h
@@ -4,8 +4,9 @@ struct message_t {
     char* message;
     struct user_t* sender;
     struct channel_t* channel;
-    struct timeval time;
+    struct tag_header_t* tag;
 };
+
 extern struct message_t create_message(struct channel_t* channel, struct user_t* sender, char* msg);
 extern void print_message_block(struct message_t* message_block);
 #endif
