a7d5bd027677538b09fc83db73d4587df164e36a
Christian Horton <christian@christianhorton.me>
2022-03-01 03:44:12 +0000

    Added comments for hashmap header

--- hashmap.h
+++ hashmap.h
@@ -1,9 +1,18 @@
 #ifndef __HASHMAP_H__
 #define __HASHMAP_H__
+
+/**
+ * @brief Bucket structure
+ *
+ */
 struct bucket_t {
     struct element_t* head;
 };
 
+/**
+ * @brief Element structure
+ *
+ */
 struct element_t {
     struct element_t* next;
 };
